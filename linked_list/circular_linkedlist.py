85b4877a08e46ce835a211fc7680c48948b05a83
Ambady Dileep <ambadydileep.pro@gmail.com>
2026-01-05 23:39:08 +0530

    Make updates in the circular and doublylinkedlist

--- linked_list/circular_linkedlist.py
+++ linked_list/circular_linkedlist.py
@@ -1,39 +1,16 @@
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
+
 
 class CircularLinkedList:
     def __init__(self):
         self.head = None
 
-    def append(self,data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-            new_node.next = self.head
-            return
-        
-        current = self.head
-        while current.next != self.head:
-            current = current.next
-        current.next = new_node
-        new_node.next = self.head
+    # ---------------- INSERTIONS ---------------- #
 
-    def print_list(self):
-        if self.head is None:
-            print("Empty list")
-            return
-
-        current = self.head
-        while True:
-            print(current.data, end="<-->")
-            current = current.next
-            if current == self.head:
-                break
-        print("(head)")
-
-    def add_to_beginning(self, data):
+    def append(self, data):
         new_node = Node(data)
 
         if self.head is None:
@@ -41,53 +18,39 @@ class CircularLinkedList:
             new_node.next = self.head
             return
 
-        # Find the last node
         current = self.head
         while current.next != self.head:
             current = current.next
 
-        # Rearrange pointers
-        new_node.next = self.head
         current.next = new_node
+        new_node.next = self.head
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            new_node.next = self.head
+            return
+
+        last = self.head
+        while last.next != self.head:
+            last = last.next
+
+        new_node.next = self.head
+        last.next = new_node
         self.head = new_node
 
-    def delete_at_beginning(self):
-        if self.head is None:
-            print("List is empty!")
-            return
-
-        if self.head.next == self.head:
-            # Only one node
-            self.head = None
-            return
-
-        # Find the last node
-        current = self.head
-        while current.next != self.head:
-            current = current.next
-
-        # Point last node to the new head
-        current.next = self.head.next
-        self.head = self.head.next
-
-    def insert_at_position(self, data, pos):
-        new_node = Node(data)
-
-        # Case 1: insert at head
+    def insert_at_position(self, pos, data):
         if pos == 0:
-            if self.head is None:
-                self.head = new_node
-                new_node.next = self.head
-            else:
-                current = self.head
-                while current.next != self.head:
-                    current = current.next
-                current.next = new_node
-                new_node.next = self.head
-                self.head = new_node
+            self.insert_at_beginning(data)
             return
 
-        # Case 2: insert at given pos
+        if self.head is None:
+            print("Position out of bounds")
+            return
+
+        new_node = Node(data)
         current = self.head
         count = 0
 
@@ -95,45 +58,49 @@ class CircularLinkedList:
             current = current.next
             count += 1
 
+        if count != pos - 1:
+            print("Position out of bounds")
+            return
+
         new_node.next = current.next
         current.next = new_node
 
-    def delete_by_value(self, value):
+    # ---------------- DELETIONS ---------------- #
+
+    def delete_at_beginning(self):
         if self.head is None:
             print("List is empty")
             return
 
+        if self.head.next == self.head:
+            self.head = None
+            return
+
+        last = self.head
+        while last.next != self.head:
+            last = last.next
+
+        self.head = self.head.next
+        last.next = self.head
+
+    def delete_at_end(self):
+        if self.head is None:
+            print("List is empty")
+            return
+
+        if self.head.next == self.head:
+            self.head = None
+            return
+
         current = self.head
-        prev = None
-
-        while True:
-            if current.data == value:
-                # Case 1: deleting head
-                if current == self.head:
-                    # only one node
-                    if self.head.next == self.head:
-                        self.head = None
-                    else:
-                        temp = self.head
-                        while temp.next != self.head:
-                            temp = temp.next
-                        temp.next = self.head.next
-                        self.head = self.head.next
-                else:
-                    prev.next = current.next
-                return
-
-            prev = current
+        while current.next.next != self.head:
             current = current.next
 
-            if current == self.head:
-                break
-
-        print(f"{value} not found")
+        current.next = self.head
 
     def delete_at_position(self, pos):
         if self.head is None:
-            print("Empty list")
+            print("List is empty")
             return
 
         if pos == 0:
@@ -153,29 +120,59 @@ class CircularLinkedList:
 
         current.next = current.next.next
 
-    def delete_at_end(self):
+    def delete_by_value(self, value):
         if self.head is None:
             print("List is empty")
             return
 
-        if self.head.next == self.head:
-            self.head = None
+        current = self.head
+        prev = None
+
+        while True:
+            if current.data == value:
+                if current == self.head:
+                    self.delete_at_beginning()
+                else:
+                    prev.next = current.next
+                return
+
+            prev = current
+            current = current.next
+
+            if current == self.head:
+                break
+
+        print(f"{value} not found")
+
+    # ---------------- DISPLAY ---------------- #
+
+    def display(self):
+        if self.head is None:
+            print("Empty list")
             return
 
         current = self.head
-        while current.next.next != self.head:
+        while True:
+            print(current.data, end=" -> ")
             current = current.next
-        current.next = self.head
+            if current == self.head:
+                break
+        print("(head)")
 
 
 cll = CircularLinkedList()
+
 cll.append(10)
 cll.append(20)
 cll.append(30)
-cll.add_to_beginning(5)
-cll.print_list()
-cll.delete_at_beginning()
-cll.print_list()
-cll.delete_at_position(2)
-cll.print_list()
 
+cll.insert_at_beginning(5)
+cll.insert_at_position(2, 15)
+
+cll.display()
+
+cll.delete_at_beginning()
+cll.delete_at_end()
+cll.delete_by_value(20)
+
+cll.display()
--- linked_list/doubly_linkedlist.py
+++ linked_list/doubly_linkedlist.py
@@ -1,152 +1,4 @@
 class Node:
-    def __init__(self,data):
-        self.data = data
-        self.next = None
-        self.prev = None
-        
-class DoublyLinkedList:
-    def __init__(self):
-        self.head = None
-        
-    def append(self,data):
-        new_node = Node(data)
-        
-        if self.head is None:
-            self.head = new_node
-            return
-        
-        current = self.head
-        while current.next:
-            current = current.next
-        current.next = new_node
-        new_node.prev = current
-        
-    def print_forward(self):
-        current = self.head
-        while current:
-            print(current.data, end="<-->")
-            current = current.next
-        print("None")
-            
-    def print_backward(self):
-        if self.head is None:
-            print("No Linkedlist exists")
-            return
-        
-        current = self.head
-        while current.next:
-            current = current.next
-        
-        while current:
-            print(current.data, end = "<-->")
-            current = current.prev
-        print("None")
-        
-    def add_to_beginning(self, data):
-        new_node = Node(data)
-        new_node.next = self.head
-        
-        if self.head is not None:
-            self.head.prev = new_node
-            
-        self.head = new_node
-        
-    def delete_at_beginning(self):
-        if self.head is None:
-            print("No Linkedlist exist's")
-            return
-        if self.head.next is None:
-            self.head = None
-        else:
-            self.head = self.head.next
-            self.head.prev = None
-        
-    def delete_at_end(self):
-        if self.head is None:
-            print("No Linkedlist found!")
-            return
-        if self.head.next is None:
-            self.head = None
-            return
-        
-        current = self.head
-        while current.next:
-            current = current.next
-        
-        current.prev.next = None    
-        
-    def insert_at_position(self,pos,data):
-        new_node = Node(data)
-        if pos == 0:
-            self.add_to_beginning(data)
-            return
-        
-        current = self.head
-        count = 0
-        
-        while current and count > pos -1:
-            current = current.next
-            count+=1
-        
-        if current is None:
-            print("Position out of bound!!")
-            return
-        
-        new_node.next = current.next
-        new_node.prev = current
-        
-        if current.next:
-            current.next.prev = new_node
-            
-        current.next = new_node
-        
-    def delete_at_position(self,pos):
-        if self.head is None:
-            print("No LinkedList exists!")
-            return
-        
-        if pos == 0:
-            self.delete_at_beginning()
-            return
-        
-        current = self.head
-        count = 0
-        
-        while current and count < pos:
-            current = current.next
-            count+=1
-            
-        if current is None:
-            print("Position out of bounds!")
-            return
-        
-        if current.prev:
-            current.prev.next = current.next
-            
-        if current.next:
-            current.next.prev = current.prev
-        
-        
-        
-dll = DoublyLinkedList()
-dll.append(20)
-dll.append(30)
-dll.append(40)
-dll.print_forward()
-dll.add_to_beginning(10)
-dll.print_forward()
-dll.print_backward()
-dll.print_forward()
-dll.delete_at_beginning()
-dll.print_forward()
-dll.delete_at_end()
-dll.print_forward()
-dll.insert_at_position(2, 25)
-dll.print_forward()
-
-# ======================================================= #
-
-class Node:
     def __init__(self, data):
         self.data = data
         self.prev = None
@@ -156,8 +8,11 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None
 
+    # ---------------- INSERTIONS ---------------- #
+
     def append(self, data):
         new_node = Node(data)
+
         if self.head is None:
             self.head = new_node
             return
@@ -172,17 +27,43 @@ class DoublyLinkedList:
     def insert_at_beginning(self, data):
         new_node = Node(data)
 
-        if self.head is None:
-            self.head = new_node
+        if self.head:
+            self.head.prev = new_node
+            new_node.next = self.head
+
+        self.head = new_node
+
+    def insert_at_position(self, pos, data):
+        if pos == 0:
+            self.insert_at_beginning(data)
             return
 
-        new_node.next = self.head
-        self.head.prev = new_node
-        self.head = new_node
+        current = self.head
+        count = 0
+
+        while current and count < pos - 1:
+            current = current.next
+            count += 1
+
+        if current is None:
+            print("Position out of bounds")
+            return
+
+        new_node = Node(data)
+        new_node.next = current.next
+        new_node.prev = current
+
+        if current.next:
+            current.next.prev = new_node
+
+        current.next = new_node
+
+    # ---------------- DELETIONS ---------------- #
 
     def delete_at_beginning(self):
         if self.head is None:
-            return "Empty"
+            print("List is empty")
+            return
 
         if self.head.next is None:
             self.head = None
@@ -193,7 +74,8 @@ class DoublyLinkedList:
 
     def delete_at_end(self):
         if self.head is None:
-            return "Empty"
+            print("List is empty")
+            return
 
         if self.head.next is None:
             self.head = None
@@ -207,10 +89,12 @@ class DoublyLinkedList:
 
     def delete_at_position(self, pos):
         if self.head is None:
-            return "Empty"
+            print("List is empty")
+            return
 
         if pos == 0:
-            return self.delete_at_beginning()
+            self.delete_at_beginning()
+            return
 
         current = self.head
         count = 0
@@ -220,80 +104,79 @@ class DoublyLinkedList:
             count += 1
 
         if current is None:
-            return "Out of range"
+            print("Position out of bounds")
+            return
 
         if current.next is None:
-            return self.delete_at_end()
+            self.delete_at_end()
+            return
 
         current.prev.next = current.next
         current.next.prev = current.prev
 
     def delete_by_value(self, value):
         if self.head is None:
-            return "Empty"
+            print("List is empty")
+            return
 
         current = self.head
 
         if current.data == value:
-            return self.delete_at_beginning()
+            self.delete_at_beginning()
+            return
 
         while current and current.data != value:
             current = current.next
 
         if current is None:
-            return "Not found"
+            print("Value not found")
+            return
 
         if current.next is None:
-            return self.delete_at_end()
+            self.delete_at_end()
+            return
 
         current.prev.next = current.next
         current.next.prev = current.prev
 
+    # ---------------- DISPLAY ---------------- #
+
     def display_forward(self):
         current = self.head
         while current:
-            print(current.data, end=" -> ")
+            print(current.data, end=" <-> ")
             current = current.next
         print("None")
 
     def display_backward(self):
-        current = self.head
-        if current is None:
-            print("Empty")
+        if self.head is None:
+            print("List is empty")
             return
 
-        # go to last
+        current = self.head
         while current.next:
             current = current.next
 
         while current:
-            print(current.data, end=" <- ")
+            print(current.data, end=" <-> ")
             current = current.prev
         print("None")
 
 
 dll = DoublyLinkedList()
-dll.append(10)
+
 dll.append(20)
 dll.append(30)
 dll.append(40)
-dll.append(50)
-dll.append(60)
+
+dll.insert_at_beginning(10)
+dll.insert_at_position(2, 25)
 
 dll.display_forward()
-dll.delete_at_end()
-dll.display_forward()
-
-dll.delete_at_position(1)
-dll.display_forward()
-
-dll.delete_by_value(20)
-dll.display_forward()
-
 dll.display_backward()
 
+dll.delete_at_beginning()
+dll.delete_at_end()
+dll.delete_by_value(25)
 
-
-
-
-
+dll.display_forward()
