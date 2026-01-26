36b5acca411005b40b6926020569a0f1e4784d3d
Ambady Dileep <ambadydileep.pro@gmail.com>
2026-01-26 23:10:25 +0530

    Selection sort revision

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -18,24 +18,19 @@
 # So it never gets faster, even if your array is already sorted
 # Example: [1, 2, 3, 4, 5] still takes the same effort as [5, 4, 3, 2, 1]
 
-# To understand this run below
+
 def selection_sort(arr):
     n = len(arr)
     for i in range(n):
         min_index = i
-        for j in range(i + 1, n):  # so in this line the next index till the count of n
-            if arr[j] < arr[min_index]:  # if the new value is less than the current smallest value
-                min_index = j  # update the index of the new smallest value
-                # ✅ arr = [4, 2, 7, 1]
-                # ✅ i = 0, min_index = 0 (you assume 4 is the smallest)
-                # ✅ Is 2 < 4? → min_index = 1
-                # ✅ Is 7 < 2? → no change
-                # ✅ Is 1 < 2? → min_index = 3
-        arr[i], arr[min_index] = arr[min_index], arr[i]  # swap smallest found with current i
+        for j in range(i + 1, n): 
+            if arr[j] < arr[min_index]: 
+                min_index = j  
+        arr[i], arr[min_index] = arr[min_index], arr[i] 
     return arr  
 
 arr = [4, 2, 7, 1]
-print(selection_sort(arr))  # ✅ Output: [1, 2, 4, 7]
+print(selection_sort(arr)) 
 
 def selection_sort(arr):
     n = len(arr)
@@ -44,6 +39,19 @@ def selection_sort(arr):
         for j in range(i+1,n):
             if arr[j] < arr[min_index]:
                 min_index = j 
+        arr[min_index],arr[i] = arr[i],arr[min_index]
+    return arr 
+
+arr = [4, 2, 7, 1]
+print(selection_sort(arr))
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i 
+        for j in range(i+1,n):
+            if arr[j] > arr[min_index]:
+                min_index = j
         arr[min_index],arr[i] = arr[i],arr[min_index]
     return arr 
 
