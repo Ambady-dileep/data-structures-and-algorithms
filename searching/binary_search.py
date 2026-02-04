914a63b240c587b01ae59c0fb279f6fda66a9c5a
Ambady Dileep <ambadydileep.pro@gmail.com>
2026-02-04 22:46:09 +0530

    Binary Search Done

--- searching/binary_search.py
+++ searching/binary_search.py
@@ -19,3 +19,26 @@ arr = [1, 4, 7, 9, 12, 18, 25, 33, 45, 50]
 target = 12
 
 print(binary_search(arr, target))
+
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high)//2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid -1
+    return -1
+    
+arr = [1, 4, 7, 9, 12, 18, 25, 33, 45, 50]
+target = 12
+
+print(binary_search(arr, target))
+
+
